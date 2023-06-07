97757ec53d51e05de1c091de08f10e32837b5ad8
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-06-07 00:25:46 -0300

    estrutura

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Serve para criar variaveis de ambiente
+  env: {
+    // process.env.teste
+    teste: 'teste'
+  }
 }
 
 module.exports = nextConfig
